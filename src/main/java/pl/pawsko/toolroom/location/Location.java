3ed5b1220f3c79df1be0b528b365abbeff5eac59
Pawel Skora <pawsko.to@gmail.com>
2022-11-10 10:37:12 +0100

    expanded * on import lines to whole import, Added missing annotation in Config

--- src/main/java/pl/pawsko/toolroom/location/Location.java
+++ src/main/java/pl/pawsko/toolroom/location/Location.java
@@ -2,7 +2,11 @@ package pl.pawsko.toolroom.location;
 
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "locations")
--- src/main/java/pl/pawsko/toolroom/powertype/PowerType.java
+++ src/main/java/pl/pawsko/toolroom/powertype/PowerType.java
@@ -2,7 +2,11 @@ package pl.pawsko.toolroom.powertype;
 
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "powerTypes")
