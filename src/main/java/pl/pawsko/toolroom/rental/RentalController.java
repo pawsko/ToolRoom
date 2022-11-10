ab5302c2938bffcbd70f75611cef3b4b3ac8e198
Pawel Skora <pawsko.to@gmail.com>
2022-11-10 11:01:08 +0100

    removed unused annotation

--- src/main/java/pl/pawsko/toolroom/rental/RentalController.java
+++ src/main/java/pl/pawsko/toolroom/rental/RentalController.java
@@ -7,22 +7,14 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
 
 @RestController
-@Slf4j
 @Tag(name = "Rentals")
 @RequestMapping("/api/rental")
 public class RentalController {
